86f3c241fabe18fb48a89c9968eb50124ad94873
Aniket Katare <113428278+aniketkatare841@users.noreply.github.com>
2023-08-20 17:52:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const ak = document.querySelector('.resume')
+ak.addEventListener('click',()=>{
+    alert('This is an Digital Resume by Aniket Katare')
+});
+
+const d = new Date();
+document.getElementById("demo").innerHTML = d;
+
+/* Just for visuals; not relevant to what's being demonstrated */
+//   .web-page {
+//     padding: 15px;
+//     display: inline-block;
+//     width: 25%;
+//     min-width: 180px;
+//     max-width: 300px;
+//     height: 300px;
+//     margin: 10px;
+//     border: 2px black solid;
+//     overflow-y: scroll;
+//     text-align: left;
+//     background-color: white;
+//   }
+  
+//   .dynamic-content {
+//     height: 100px;
+//     width: 100%;
+//     background-color: #0D47A1;
+//     color: #eee;
+//     text-align: center;
+//     line-height: 100px;
+//     font-size: 16px;
+//     font-weight: bold;
+//   }
+
